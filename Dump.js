4071370946b8f875bbcdebd20389fc1194040baa
spences10 <spences10apps@gmail.com>
2019-10-23 19:47:05 +0100

    Prettier

--- Dump.js
+++ Dump.js
@@ -11,7 +11,9 @@ const Dump = props => (
   >
     {Object.entries(props).map(([key, val]) => (
       <pre key={key}>
-        <strong style={{ color: 'white', background: 'red' }}>{key} 💩</strong>
+        <strong style={{ color: 'white', background: 'red' }}>
+          {key} 💩
+        </strong>
         {JSON.stringify(val, '', ' ')}
       </pre>
     ))}
